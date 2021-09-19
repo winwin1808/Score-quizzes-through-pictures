f9658df6925f757a315cbe61b2e527a745f37ea1
= <nguyenquanghao1808@gmail.com>
2021-09-20 02:43:50 +0700

    fix

--- CODE.py
+++ CODE.py
@@ -1,4 +1,3 @@
-from os import sep
 import cv2
 import pandas as pd 
 import matplotlib.pyplot as plt
@@ -6,7 +5,6 @@ import glob
 import imutils
 import numpy as np
 import os.path
-import csv
 
 chars = ['A', 'B', 'C', 'D', 'E']
 answer_per_question = 5 
@@ -25,19 +23,8 @@ height = 842
 
 #################################
 # Import data
-
-# data = ['DATA/'+ 
-# '20012311_TranDuc_3A.png',
-# '20012312_QuyLan_3A.png',
-# '20012313_HoangGiaUy_3A.png',
-# '20012317_NguyenQuangHao_3A.png',
-# '20012318_NguyenVanC_3A.png',
-# '20012319_NguyenThiNo_3A.png']
-
 img_list = list()
 student_data_list = list()
-# os.chdir("D:\LEARN\Python\Problemset_2\-VNUK-Challenge_2_HAO_UY\DATA")
-# path = glob.glob("DATA/*.png")
 
 # images = glob.glob("DATA/*.png")
 images = [os.path.basename(x) for x in glob.glob("-VNUK-Challenge_2_HAO_UY/DATA/*.png")]
@@ -60,30 +47,32 @@ def show_images(titles, images, wait=True):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-
 def threshold_img(img):
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   _, thresh = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY_INV)  
   return thresh
 
 def crop_per_part(img,part):
-
-  part = int(part)
-  dis = 70
-  xa= 10
-  h = 237
-  
+  dis = 75
+  xa= 6
+  h = 161
   if part == 1:
-    img_crop = img[(h+int(dis*(part-1))):(h+int(dis*part)),150:275]
+    img_crop = img[(h+(dis*(part-1))):(h+(dis*part)),150:275]
     return img_crop
-  elif 1 < part < 7:
-    img_crop = img[(h+int(dis*(part-1))+xa):(h+int(dis*part)+xa),150:275]  
+  elif 1 < part < 6:
+    img_crop = img[(h+(dis*(part-1))+xa):(h+(dis*part)+xa),150:275]  
     return img_crop
+  elif part == 6:
+    img_crop = img[(h+(dis*(part-1))+xa+10):(h+(dis*part)+xa+10),150:275]  
+    return img_crop 
   elif part == 7:
-    img_crop = img[(h+int(dis*(part-7))):(h+int(dis*(part-6))),350:475]
+    img_crop = img[(h+(dis*(part-7))):(h+(dis*(part-6))),350:475]
     return img_crop
-  elif 6 < part < 14:
-    img_crop = img[(h+int(dis*(part-7))+xa):(h+int(dis*(part-6))+xa),350:475]
+  elif 7 < part < 12:
+    img_crop = img[(h+(dis*(part-7))+xa):(h+(dis*(part-6))+xa),350:475]
+    return img_crop
+  elif part == 12:
+    img_crop = img[(h+(dis*(part-7))+xa+15):(h+(dis*(part-6))+xa+15),350:475]
     return img_crop
   else:
     return img
@@ -121,7 +110,6 @@ def find_index(myPixelVal):
 
 def grading(index,answer):
   grading = []
-  
   for x in range (0,questions_per_part):
     if answer[x] == index[x]:
       grading.append(1)
@@ -139,13 +127,39 @@ def get_answer(i):
   answer_key = []
   x = 1
   while x < 13:
-    thres = threshold_img(crop_per_part(answer,x))
+    image = crop_per_part(answer,x)
+    thres = threshold_img(image)
     boxes = split_image (thres)
     pixel_value = check_answer(boxes)
     index = find_index(pixel_value)
     answer_key.append(index)
     x += 1  
   return answer_key[i]
+# print(get_answer(0))
+
+def test_graded(img_index):
+  int(img_index)
+  graded = []
+  # char =[]
+  x = 1
+  while x < 13:
+    #Total_part
+    answer_index = [] 
+    image = crop_per_part(img_list[img_index],x)
+    thres = threshold_img(image)
+    boxes = split_image (thres)
+    pixel_value = check_answer(boxes)
+    index = find_index(pixel_value)
+    answer_index.append(index)
+    answer_key = get_answer(x-1)
+    grade = grading(answer_index[0],answer_key)
+    graded.append(grade)
+    x +=1
+  graded = np.concatenate(graded)
+  score = score_show(graded)
+  return score
+
+# test_graded(12)
 
 def process_chars(index):
   char = []
@@ -155,40 +169,12 @@ def process_chars(index):
         index[x] = chars[y]
   return index 
 
-
-def test_graded(img_index):
-  graded = []
-  char =[]
-  x = 1
-  while x < 13:
-    answer_index = [] 
-    thres = threshold_img(crop_per_part(img_list[img_index],x))
-    boxes = split_image (thres)
-    pixel_value = check_answer(boxes)
-    index = find_index(pixel_value)
-    answer_index.append(index)
-    # char.append(process_chars(answer_index[0]))
-    grade = grading(answer_index[0],get_answer(x-1))
-    graded.append(grade)
-    x +=1
-  # process_chars(answer_index)
-  graded = np.concatenate(graded)
-  # print(graded)
-  score = score_show(graded)
-  return score
-
-
-def correct_questions():
-  all_score = list()
-  for i in range (0,len(img_list)):     
-    all_score.append(test_graded(i))
-  return all_score  
 ########################################################################
 # add_dataframe()
 ########################################################################
 # Ex2: Create CSV file:
 df_info = pd.DataFrame(student_data_list, columns=['Student ID','Name','Test Code'])
-# df_info.to_csv('student_INFO.csv')
+df_info.to_csv('student_INFO.csv')
 ########################################################################
 #Ex3: Generating the first 5 answers of one student:
 def first_five():
@@ -229,34 +215,49 @@ def all_answer():
 
 ######################################################################## 
 #Ex5: Generating grading.csv
-correct_questions = correct_questions()
+def correct_questions():
+  all_score = list()
+  for i in range (0,len(img_list)):     
+    all_score.append(test_graded(i))
+  return all_score  
+
 def grading_csv():
-  df_Score = pd.DataFrame({'Score':correct_questions})
+  correct = correct_questions()
+  df_Score = pd.DataFrame({'Score':correct})
   df_ID = pd.DataFrame({'Student ID':df_info['Student ID']})
   frame = [df_ID,df_Score]
   grading = pd.concat(frame,axis=1)
   grading.to_csv('grading.csv')
-# grading_csv()
+grading_csv()
 ########################################################################
 #Ex6: Summary which 3 questions are the most 
 
 ########################################################################
 #Ex7: Generating the final result (pass/fail) of the class
-conv_correct_questions = [float(x) for x in correct_questions]
+def conv_ques():
+  conv_correct_questions = [float(x) for x in correct_questions()]
+  return conv_correct_questions
 def final_result():
   all_result = list()
+  correct = conv_ques()
   for i in range (0,len(img_list)):
-    if conv_correct_questions[i]>8:
+    if correct[i]>8:
       all_result.append('Pass')
     else:
       all_result.append('Fail')
   return all_result
 
 def result_csv():
+  correct = correct_questions()
   result = final_result()
   df_Result = pd.DataFrame(result, columns=['Result'])
-  df_Score = pd.DataFrame({'Score':correct_questions})
+  df_Score = pd.DataFrame({'Score':correct})
   frame = [df_info, df_Score, df_Result]
   df_final = pd.concat(frame,axis=1)
   df_final.to_csv('Final Result.csv')
 result_csv()
+img = crop_per_part(img_list[0],12) 
+boxes = split_image (img)
+cv2.imshow('Test',img)
+cv2.imshow('image',img_list[0])
+cv2.waitKey(0)
